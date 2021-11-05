c90d73a1e7f5573d0f4403c393ab47f7901c7104
George Makarowski <george.makarowski@yara.com>
2021-11-05 23:52:51 +0100

    Test working

--- conftest.py
+++ conftest.py
@@ -4,18 +4,18 @@ import pytest
 from selenium import webdriver
 
 
-@pytest.fixture
-def browser():
-    browser = webdriver.Chrome()
-    yield browser
-    browser.quit()
-
-
 def pytest_addoption(parser):
-    parser.addoption("--language", action="store", default="en")
+    parser.addoption("--language", action="store", default="en-gb")
 
 
 def pytest_generate_tests(metafunc):
     option_value = metafunc.config.option.language
     if 'language' in metafunc.fixturenames and option_value is not None:
         metafunc.parametrize("language", [option_value])
+
+
+@pytest.fixture
+def browser():
+    browser = webdriver.Chrome()
+    yield browser
+    browser.quit()
