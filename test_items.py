b37fe26f1f5fa3dc356f7224851e577d1d134160
George Makarowski <george.makarowski@yara.com>
2021-11-05 23:54:03 +0100

    Tests updated

--- test_items.py
+++ test_items.py
@@ -1,18 +1,50 @@
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium import webdriver
+from selenium.webdriver.support.ui import Select
 
 
 def test_page(browser, language):
     # given
-    print(f'Using language {language}')
+    expected_texts = {
+        'ar': 'أضف الى سلة التسوق',
+        'ca': 'Afegeix a la cistella',
+        'cs': 'Vložit do košíku',
+        'da': 'Læg i kurv',
+        'de': 'In Warenkorb legen',
+        'en-gb': 'Add to basket',
+        'el': 'Προσθήκη στο καλάθι',
+        'es': 'Añadir al carrito',
+        'fi': 'Lisää koriin',
+        'fr': 'Ajouter au panier',
+        'it': 'Aggiungi al carrello',
+        'ko': '장바구니 담기',
+        'nl': 'Voeg aan winkelmand toe',
+        'pl': 'Dodaj do koszyka',
+        'pt': 'Adicionar ao carrinho',
+        'pt-br': 'Adicionar à cesta',
+        'ro': 'Adauga in cos',
+        'ru': 'Добавить в корзину',
+        'sk': 'Pridať do košíka',
+        'uk': 'Додати в кошик',
+        # this lang doesn't work
+        'zh-hans': 'Add to basket',
+    }
     url = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
     # when
     browser.get(url)
 
-    # then
-    WebDriverWait(browser, 12).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, "button.btn"))
+    select = Select(browser.find_element_by_tag_name('select[name="language"]'))
+    select.select_by_value(language)
+
+    lang_btn = browser.find_element_by_xpath('//*[@id="language_selector"]/button')
+    lang_btn.click()
+
+    add_btn = WebDriverWait(browser, 12).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="add_to_basket_form"]/button'))
     )
+    actual_text = add_btn.text
+
+    assert actual_text == expected_texts[language]
